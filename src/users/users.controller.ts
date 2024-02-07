5489b8a84b029cfa771754db074882aadd1c8b8e
Piotr Ranosz <pranosz@wp.pl>
2024-02-07 21:36:10 +0100

    Mock API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,12 +8,16 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from 'src/model/user.interface';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get() // GET /users OR /users?role=value
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'): string[] {
-    return ['user1', 'user2', role];
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'): User[] {
+    return this.usersService.findAll(role);
   }
   /*
   @Get('interns') // GET /users/interns
@@ -22,22 +26,22 @@ export class UsersController {
   }
   */
   @Get(':id') // GET /users/:id
-  findOne(@Param('id') id: string) {
-    return { id };
+  findOne(@Param('id') id: string): User {
+    return this.usersService.findOne(id);
   }
 
   @Post() // POST /users
-  create(@Body() user: { name: string }) {
-    return user;
+  create(@Body() user: Omit<User, 'id'>): User {
+    return this.usersService.create(user);
   }
 
   @Patch(':id') // Patch /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: object) {
-    return userUpdate;
+  update(@Param('id') id: string, @Body() userUpdate: Partial<User>) {
+    return this.usersService.update(id, userUpdate);
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return id;
+    return this.usersService.delete(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,67 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/model/user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      id: 1,
+      name: 'Piotr',
+      email: 'heja@wp.pl',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Kot',
+      email: 'hekot@onet.plja@wp.pl',
+      role: 'ADMIN',
+    },
+    {
+      id: 3,
+      name: 'Lena',
+      email: 'Lena@wp.pl',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'Robert',
+      email: 'rob@wp.pl',
+      role: 'ENGINEER',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN'): User[] {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    } else {
+      return this.users;
+    }
+  }
+
+  findOne(id: string): User {
+    return this.users.find((user) => user.id === Number(id));
+  }
+
+  create(user: Omit<User, 'id'>): User {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = { ...user, id: usersByHighestId[0].id + 1 };
+    this.users = [...this.users, newUser];
+
+    return newUser;
+  }
+
+  update(id: string, userUpdate: Partial<User>): void {
+    const users = this.users.map((user) => {
+      if (user.id === Number(id)) {
+        return { ...user, ...userUpdate };
+      }
+      return user;
+    });
+    this.users = [...users];
+  }
+
+  delete(id: string): void {
+    const newUsers = this.users.filter((user) => user.id !== Number(id));
+    this.users = [...newUsers];
+  }
+}

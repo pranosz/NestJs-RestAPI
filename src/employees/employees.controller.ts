7487b85d98798d4362df5c8563edf3de15f580af
Piotr Ranosz <pranosz@wp.pl>
2024-02-10 20:46:39 +0100

    Rate Limits

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -10,16 +10,20 @@ import {
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
+  @SkipThrottle({ default: false })
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @Throttle({ short: { limit: 1, ttl: 1000 } })
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     return this.employeesService.findAll(role);
@@ -30,6 +34,7 @@ export class EmployeesController {
     return this.employeesService.findOne(+id);
   }
 
+  @SkipThrottle({ default: false })
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -38,6 +43,7 @@ export class EmployeesController {
     return this.employeesService.update(+id, updateEmployeeDto);
   }
 
+  @SkipThrottle({ default: false })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.employeesService.remove(+id);
